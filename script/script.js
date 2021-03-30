ce6037892972dde88a403dc72af4570dcbc25e8a
all persons <35461002238898@mail.ru>
2021-03-30 06:30:16 +0300

    add new file ADDuserfn.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
 'use strict'
-import {data} from './data.js'
+import {data} from './data.js';
+import {setData} from './addUserfn.js';
 
 const userAddBtn = document.getElementById('userAddBtn');
 const modalWindow = document.getElementById('modal');
 const modalCancelBtn = document.getElementById('modalCancelBtn');
-const userData = document.querySelectorAll('.user_data_item');
 const userContainer = document.getElementById('userContainer');
-let userDataArray = Array.from(userData);
-let newUserData = [];
+const modalWindowAddBtn = document.getElementById('modalAddBtn');
 
 function setUserData(array) {
     let containerItemName = document.createElement('div');
@@ -21,27 +20,24 @@ function setUserData(array) {
     userContainer.append(containerItemName);
     userContainer.append(containerItemLastName);
     userContainer.append(containerItemAge);
-
-    console.log(array)
 };
 
 setUserData(data)
 
-
-// найти инпуты, получить их вэлью, запихинуть в объект и по клику на кнопку этот объект пушить в массив
-
-
-// function setUserData() {
-//     const divElement = document.createElement('div');
-//     userContainer.append(divElement);
-//     divElement.insertAdjacentHTML('afterend', `<div class='item'> ${data[0].firstName} </div>`);
-//     divElement.insertAdjacentHTML('afterend', `<div class='item'> ${data[1].firstName} </div>`);
-//     divElement.insertAdjacentHTML('afterend', `<div class='item'> ${data[2].firstName} </div>`);
-    
-    // divElement.insertAdjacentHTML('afterbegin', `<div class='modal_item'> ${data[3].lastName} </div>`);
-// }
-
+// получить их вэлью, запихинуть в объект и по клику на кнопку этот объект пушить в массив
 
 modalCancelBtn.addEventListener('click', () => modalWindow.classList.add('hide'));
 
-userAddBtn.addEventListener('click', () => modalWindow.classList.remove('hide'));
+userAddBtn.addEventListener('click', () => {
+    modalWindow.classList.remove('hide')
+    }
+);
+
+modalWindowAddBtn.addEventListener('click', ()=>  {
+    data.push(setData);
+    setData()
+});
+
+// modalCancelBtn.addEventListener('click', () => modalWindow.hidden = true);
+
+// userAddBtn.addEventListener('click', () => modalWindow.hidden = false );
